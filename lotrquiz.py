4509eae7241656392cdbea39f2ad703d1771c068
johnstonm-coder <johnstonm@stu.otc.school.nz>
2026-04-22 14:49:54 +1200

    day1test

---
+++ lotrquiz.py
@@ -0,0 +1,15 @@
+'''
+Mitchell Johnston 20/4/2026
+This is my code about the lord of the rings and new zealand, the questions are multichoice so the user has a more fair chance of getting the question correct'''
+theiranswers = []
+
+print("What town in New Zealand were the hobbits homes created?")
+print("1. Matamata")
+print("2. Tauranga")
+print("3. Auckland")
+print("4. Dunedin")
+answer = int(input("Select the number that you think is the answer: "))
+if answer == 1:
+    theiranswers.append(1)
+else:
+    theiranswers.append(0)
