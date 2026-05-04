f05ecb83b4fecd0929966c4f59e0103c84701673
johnstonm-coder <johnstonm@stu.otc.school.nz>
2026-05-05 10:57:48 +1200

    day 6 small change

--- lotrquiz.py
+++ lotrquiz.py
@@ -4,19 +4,21 @@ This is my code about the lord of the rings and new zealand, the questions are
  multichoice so the user has a more fair chance of getting the question correct
 '''
 #this is the list of all the questions
-questions = ["What town in New Zealand were the hobbits homes created?", 
+questions = ["Where are the hobbits homes located in New Zealand?", 
             "What did Gollum call the one ring?",
             "What race are the main characters in the movies?",
             "What is the name of the big battle in the third film?", 
             "Where is Mt doom/Mordor in NZ?", 
             "How many films are in the LOTR series?", 
             "What is the name of the Dark Lord?", 
-            "What Mountain is the filming location for the Edoras (The Capital city of Rohan) in Lord Of The Rings",]
+            "What Mountain is the filming location for the Edoras"
+            " (The Capital city of Rohan) in Lord Of The Rings",]
 #this is the list of all the answers 
 options = ["1. Matamata\n2. Tauranga\n3. Auckland\n4. Dunedin", 
         "1. His Ring\n2. His Wife\n3. His Precious\n4. His Favourite\n",
         "1. Elf\n2. Hobbit\n3. Dwarf\n4. Orc\n",
-        "1. Battle of Helm's Deep\n2. Battle of Moscow\n3. Battle of Gate Pa\n4. Battle of the Pelennor Fields\n",
+        "1. Battle of Helm's Deep\n2. Battle of Moscow\n3. Battle of Gate Pa"
+        "\n4. Battle of the Pelennor Fields\n",
         "1. Mt cook\n2. Mt Ngauruhoe\n3. Taranaki\n4. Mt Maunganui\n",
         "1. 3\n2. 4\n3. 8\n4. 1\n",
         "1. Sam Wise\n2. Sauron\n3. Hitler\n4. Gandalf\n",
@@ -31,6 +33,7 @@ questionno = 0
 for qst in questions:
     print(questions[questionno])
     print(options[questionno])
+    print("")
     answer = input("Select the number that you think is correct: ")
     #check if the user enters a number and repeats the loop until they enter one
     while True:
@@ -48,10 +51,14 @@ for qst in questions:
     #add score if the answer is correct
     if answer_int == Correctanswer[questionno]:
         score = score + 1
+        print("")
         print("Correct Good Job!")
+        print("")
     else:
         wrongscore = wrongscore + 1
+        print("")
         print("Close but not quite, you'll get it next time!")
+        print("")
 
     questionno = questionno + 1
 #calculates the percentage the user gets right and print out a comment based on
